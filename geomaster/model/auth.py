28aa18bca3e3896fed34b5a857521940d6a193a5
kapur498 <rohan.kapur498@gmail.com>
2020-10-13 11:46:46 +0000

    Add session and auth to model

---
+++ geomaster/model/auth.py
@@ -0,0 +1,152 @@
+# -*- coding: utf-8 -*-
+"""
+Auth* related model.
+This is where the models used by the authentication stack are defined.
+It's perfectly fine to re-use this definition in the geomaster application,
+though.
+"""
+import os
+from datetime import datetime
+from hashlib import sha256
+__all__ = ['User', 'Group', 'Permission', 'GeoMaster']
+
+from ming import schema as s
+from ming.odm import FieldProperty, ForeignIdProperty, RelationProperty
+from ming.odm import Mapper
+from ming.odm.declarative import MappedClass
+from geomaster.model import DBSession
+
+class GeoMaster(MappedClass):
+    """
+    GeoMaster definition.
+    """
+    class __mongometa__:
+        session = DBSession
+        name = 'geomaster'
+        indexes = [('country', ), ('country_code', )]
+        unique_indexes = [('range1',), ('range2',)]
+
+    def __json__(self):
+        return dict(country=self.country, country_code=self.country_code,
+                    region=self.region, city=self.city, latitude=self.lat,
+                    longitude=self.lng, zipcode=self.zipcode, timezone=self.timezone)
+
+
+    _id = FieldProperty(s.ObjectId)
+    range1 = FieldProperty(s.Int)
+    range2 = FieldProperty(s.Int)
+    country = FieldProperty(s.String)
+    country_code = FieldProperty(s.String)
+    region = FieldProperty(s.String)
+    city = FieldProperty(s.String)
+    lat = FieldProperty(s.String)
+    lng = FieldProperty(s.String)
+    zipcode = FieldProperty(s.String)
+    timezone = FieldProperty(s.String)
+
+    @classmethod
+    def getRange(cls, decimal_ip):
+        return cls.query.find({"range1": {"$lt": decimal_ip},
+                              "range2": {"$gt": decimal_ip}}).first()
+
+class Group(MappedClass):
+    """
+    Group definition.
+    """
+    class __mongometa__:
+        session = DBSession
+        name = 'tg_group'
+        unique_indexes = [('group_name',),]
+
+    _id = FieldProperty(s.ObjectId)
+    group_name = FieldProperty(s.String)
+    display_name = FieldProperty(s.String)
+
+    permissions = RelationProperty('Permission')
+
+
+class Permission(MappedClass):
+    """
+    Permission definition.
+    """
+    class __mongometa__:
+        session = DBSession
+        name = 'tg_permission'
+        unique_indexes = [('permission_name',),]
+
+    _id = FieldProperty(s.ObjectId)
+    permission_name = FieldProperty(s.String)
+    description = FieldProperty(s.String)
+
+    _groups = ForeignIdProperty(Group, uselist=True)
+    groups = RelationProperty(Group)
+
+
+class User(MappedClass):
+    """
+    User definition.
+    This is the user definition used by :mod:`repoze.who`, which requires at
+    least the ``user_name`` column.
+    """
+    class __mongometa__:
+        session = DBSession
+        name = 'tg_user'
+        unique_indexes = [('user_name',),]
+
+    class PasswordProperty(FieldProperty):
+        @classmethod
+        def _hash_password(cls, password):
+            salt = sha256()
+            salt.update(os.urandom(60))
+            salt = salt.hexdigest()
+
+            hash = sha256()
+            # Make sure password is a str because we cannot hash unicode objects
+            hash.update((password + salt).encode('utf-8'))
+            hash = hash.hexdigest()
+
+            password = salt + hash
+
+            # Make sure the hashed password is a unicode object at the end of the
+            # process because SQLAlchemy _wants_ unicode objects for Unicode cols
+            password = password.decode('utf-8')
+
+            return password
+
+        def __set__(self, instance, value):
+            value = self._hash_password(value)
+            return FieldProperty.__set__(self, instance, value)
+
+    _id = FieldProperty(s.ObjectId)
+    user_name = FieldProperty(s.String)
+    email_address = FieldProperty(s.String)
+    display_name = FieldProperty(s.String)
+
+    _groups = ForeignIdProperty(Group, uselist=True)
+    groups = RelationProperty(Group)
+
+    password = PasswordProperty(s.String)
+    created = FieldProperty(s.DateTime, if_missing=datetime.now)
+
+    @property
+    def permissions(self):
+        return Permission.query.find(dict(_groups={'$in':self._groups})).all()
+
+    @classmethod
+    def by_email_address(cls, email):
+        """Return the user object whose email address is ``email``."""
+        return cls.query.get(email_address=email)
+
+    def validate_password(self, password):
+        """
+        Check the password against existing credentials.
+        :param password: the password that was provided by the user to
+            try and authenticate. This is the clear text version that we will
+            need to match against the hashed one in the database.
+        :type password: unicode object.
+        :return: Whether the password is valid.
+        :rtype: bool
+        """
+        hash = sha256()
+        hash.update((password + self.password[:64]).encode('utf-8'))
+        return self.password[64:] == hash.hexdigest()
